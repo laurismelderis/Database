99d8835c109aa43531325d63d7627ad6458c5b2c
laurispc <laurismelderis>
2020-03-04 20:11:03 +0200

    Add word deletion from file

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,12 @@ import database.*;
 public class Main {
 	public static void main(String []args) throws Exception{
 		Scanner sc = new Scanner(System.in);
-		String txtFileName = "database3";
+		String txtFileName = "database.txt";
 		Database db = new Database();
-		// System.out.println(db.fileList);
-		db.init(txtFileName + ".txt");
-		String []vardi = db.getText(db.readText());
-		System.out.println(db.readText());
-		db.deleteText("Sveiki");
+		db.init(txtFileName);
+		String []vardi = db.getText();
+		// System.out.println(db.readText(db.file));
+		db.deleteText("id");
 	}
 	public static void output(String []arr){
 		for (String a: arr){
