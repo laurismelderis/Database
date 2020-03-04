4773d9b13b4642476b4e28433ec3c59266152e2a
Lauris <59650246+laurismelderis@users.noreply.github.com>
2020-03-05 00:17:34 +0200

    Add files via upload

--- database/Database.java
+++ database/Database.java
@@ -11,18 +11,28 @@ import java.nio.file.FileSystems;
 
 public class Database{
 	private BufferedReader br = null;
-	private File file = null;
-	public Boolean streaming = false;
+	public File file = null;
+	public boolean streaming = false;
 	public String filePath = "textFiles/";
 	public String fileList = "";
 	public String fileName = "";
 	public Database(){
 		fileList();
 	}
-	public void init(String txtName){
+	public void init(String txtName) throws IOException{
 		fileName = txtName;
 		file = new File(filePath + fileName);
-		openStream(txtName);
+		if (file.exists()){
+			openStream();
+		} else {
+			closeStream();
+			System.out.println("Datu baze neeksiste!");
+		}
+		if (checkText() == false){
+			closeStream();
+			System.out.println("Datu baze neatbilst formatam vai ir tuksa!");
+		}
+		
 	}
 	public void fileList(){
 		String list = "";
@@ -45,12 +55,12 @@ public class Database{
 		}
 		return text;
 	}
-	public void writeText(String text) throws Exception{
+	public void writeText(String text) throws IOException{
 		FileWriter fw = new FileWriter(file);
 		fw.write(text);
 		fw.flush();
 	}
-	public void deleteText(String text) throws Exception{
+	public void deleteText(String text) throws IOException{
 		File newFile = new File(filePath + "/CopyOf" + fileName);
 		FileWriter fw = new FileWriter(newFile);
 		String []words = getText(readText());
@@ -69,19 +79,48 @@ public class Database{
 		String[] words = text.split("\\s");
 		return words;
 	}
-	public void openStream(String txtName){
+	public void openStream(){
 		try{
-			this.br = new BufferedReader(new FileReader(file));
+			br = new BufferedReader(new FileReader(file));
 			streaming = true;
 		} catch (Exception e){
 			streaming = false;
 			System.out.println("File open stream error");
 		}
-		System.out.println("Stream started");
 	}
-	public void closeStream() throws Exception{
-		if (this.br != null){
-			this.br.close();
+	public void closeStream() throws IOException{
+		if (br != null){
+			br.close();
 		}
+		streaming = false;
+	}
+	public void outputTable() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		System.out.println("+-------------------------------------------------------------------------------------------------------------------------+");
+		System.out.println("|ID    |AUTORS          |NOSAUKUMS                      |UZ_VIETAS |LASITAJA_KODS |GRAMATAS_NEMEJS     |ATGRIESANAS_DATUMS|");
+		System.out.println("+-------------------------------------------------------------------------------------------------------------------------+");
+		String add = "";
+		while((add = br.readLine()) != null){
+			String []vec = add.split("\\s");
+			System.out.printf("|%-5s |%-15s |%-30s |%-9s |%-13s |%-19s |%-17s |\n",
+				vec[0], vec[1], vec[2], vec[3], vec[4], vec[5], vec[6]);
+		}
+		System.out.println("+-------------------------------------------------------------------------------------------------------------------------+");
+		br.close();
+	}
+	public boolean checkText() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		String add = br.readLine();
+		String []vec = add.split("\\s");
+		if (vec.length == 7){
+			return true;
+		}
+		return false;
+	}
+	public void output(String []arr){
+		for (String a: arr){
+			System.out.print(a);
+		}
+		System.out.println();
 	}
 }
